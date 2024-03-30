fa77b0a997262ea571cd89d8660d69670cafaa7e
sreenilay0909 <155079562+sreenilay0909@users.noreply.github.com>
2024-03-30 20:24:50 +0530

    Update and rename README.md to finding the leap year.py

---
+++ finding the leap year.py
@@ -0,0 +1,20 @@
+# it helps 
+import math
+import os
+import random
+import re
+import sys
+
+def is_leap(year):
+    leap=False
+    
+    if year%400==0:
+        leap=True
+    elif year%100==0:
+        leap=False
+    elif year%4==0:
+        leap=True
+    return leap
+
+year = int(input())
+print(is_leap(year))
